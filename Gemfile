767e81d9444cf363d9436f5c93723eba069c29ec
David Sarmiento <edst83@gmail.com>
2023-10-18 12:35:27 -0600

    add devise and google oauth

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'byebug'
 end
 
 group :development do
@@ -70,3 +71,6 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+gem 'devise', '~> 4.9', '>= 4.9.3'
+gem 'omniauth-google-oauth2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  get 'home/index'
+
+  root to: 'home#index'
 end
